b89f6bfb1e05c4c7b01a3255ff88d4a230ff171c
Ezeddin Ahmed <hadouezeddin@gmail.com>
2022-08-12 14:54:00 +0000

    create splash screen and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_todo_app/layout/home_layout.dart';
+import 'package:test_todo_app/shared/styles/my_Theme.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -10,8 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: Home.routeName,
       routes:{ Home.routeName :(context) => Home()},
+      theme: MyTheme.lightMode,
+      darkTheme: MyTheme.darkMode,
     
     );
   }
