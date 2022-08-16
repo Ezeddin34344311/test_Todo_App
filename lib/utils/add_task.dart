f697afae181db216aa08bc3024a6bc65b1004f3a
Ezeddin Ahmed <hadouezeddin@gmail.com>
2022-08-16 11:27:51 +0000

    Add Focution To Write data

--- lib/utils/add_task.dart
+++ lib/utils/add_task.dart
@@ -1,2 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/tasks_model.dart';
 
+/// Fonction Add Collection
 
+CollectionReference<TasksModel> addCollectionForFirestore() {
+  return FirebaseFirestore.instance
+      .collection('task')
+      .withConverter<TasksModel>(fromFirestore: ((snapshot, options) {
+    return TasksModel.fromJson(snapshot.data()!);
+  }), toFirestore: ((task, options) {
+    return task.toJson();
+  }));
+}
+
+/// Fonction Add Document and write Data
+
+Future<void> addTasksFromFirestore(TasksModel task) {
+  var docRef = addCollectionForFirestore().doc();
+  task.id = docRef.id;
+  return docRef.set(task);
+}
